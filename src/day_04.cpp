550acc69828d7dc2fdfbcccaeb5150172ce59099
Herwig Letofsky <herwig.m.letofsky@gmail.com>
2022-12-04 08:47:33 +0100

    add day 04 solution

--- src/day_04.cpp
+++ src/day_04.cpp
@@ -3,9 +3,18 @@
 namespace day_04
 {
 
+auto create_sections(std::string const& range) -> std::vector<u64>
+{
+  u64 start = stoul(range.substr(0, range.find('-')));
+  u64 end = stoul(range.substr(range.find('-') + 1));
+  std::vector<u64> sections(end - start + 1);
+  std::iota(sections.begin(), sections.end(), start);
+  return sections;
+}
+
 u64 part1(std::string const& file_name)
 {
-  u64 total_sum{0};
+  u64 number_of_pairs_that_fully_overlap{0};
 
   std::fstream in(file_name);
   std::string text;
@@ -13,15 +22,29 @@ u64 part1(std::string const& file_name)
     if (text.empty()) {
     }
     else {
+      std::string range1 = text.substr(0, text.find(','));
+      std::string range2 = text.substr(text.find(',') + 1);
+
+      auto sections1 = create_sections(range1);
+      auto sections2 = create_sections(range2);
+
+      if (std::includes(sections1.begin(), sections1.end(), sections2.begin(), sections2.end())) {
+        number_of_pairs_that_fully_overlap += 1;
+      }
+      else {
+        if (std::includes(sections2.begin(), sections2.end(), sections1.begin(), sections1.end())) {
+          number_of_pairs_that_fully_overlap += 1;
+        }
+      }
     }
   }
 
-  return total_sum;
+  return number_of_pairs_that_fully_overlap;
 }
 
 u64 part2(std::string const& file_name)
 {
-  u64 total_sum{0};
+  u64 number_of_pairs_that_overlap{0};
 
   std::fstream in(file_name);
   std::string text;
@@ -29,10 +52,21 @@ u64 part2(std::string const& file_name)
     if (text.empty()) {
     }
     else {
+      std::string range1 = text.substr(0, text.find(','));
+      std::string range2 = text.substr(text.find(',') + 1);
+
+      auto sections1 = create_sections(range1);
+      auto sections2 = create_sections(range2);
+
+      vector<u64> difference;
+      std::set_intersection(sections1.begin(), sections1.end(), sections2.begin(), sections2.end(), std::back_inserter(difference));
+      if (!difference.empty()) {
+        number_of_pairs_that_overlap += 1;
+      }
     }
   }
 
-  return total_sum;
+  return number_of_pairs_that_overlap;
 }
 
 Solution solve(std::string const& file_name)
